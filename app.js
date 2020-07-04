8754195d5d544685cc45452a1a0f3cf9ad84bff1
Eudes Guzmán <eudesgg19@gmail.com>
2020-07-04 13:42:51 +0000

    ejercicio terminado

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var pronouns = ['the','our'];
 var adjs = ['great', 'big' ];
 var nouns = ['jogger','racoon', 'acom' ];
-var extensions = ['.com', '.net', '.us', '.io'];
-//var extensions = ['.com', '.net', '.us', '.io', '.vip', '.app', '.xyz', '.online', '.club', '.site', '.space', '.live', '.shop', '.store'];
+var extensions = ['.com', '.net', '.us', '.io', '.vip', '.app', '.xyz', '.online', '.club', '.site', '.space', '.live', '.shop', '.store'];
 let lista =[];
 
 for (let pronoun of pronouns) {
@@ -13,26 +12,18 @@ for (let pronoun of pronouns) {
                 lista = pronoun.concat(adj,noun,extension);
                // console.log(lista);
 
-/*
-                if(noun.slice(-3) == extension.slice(1)){
+                /*
+                console.log("noun --> " + noun);
+                console.log("extension.length-1  --> " + parseInt(extension.length-1)  );
+                console.log("noun.slice(extension.length-1) --> " + noun.slice(-(extension.length-1))); *******
+                console.log("noun.slice(-3) --> " + noun.slice(-3));
+                console.log("*****************");
+                */         
+                if(noun.slice(-(extension.length-1)) == extension.slice(1)){
                     console.log(pronoun.concat(adj,noun.slice(0,-3).concat(extension)));  
                 }
                 else{
                     console.log(pronoun.concat(adj,noun,extension));
-                }
-*/         
-
-                //console.log(noun.slice(extension.length-1));
-                console.log("noun --> " + noun);
-                console.log("extension.length-1  --> " + parseInt(extension.length-1)  );
-                console.log("noun.slice(extension.length-1) --> " + noun.slice(extension.length-1));
-                 console.log("*****************");
-                           
-                
-
-
-                if(noun.slice(extension.length-1) == extension.slice(1)){
-                    console.log("hola");
                 }
 
 
